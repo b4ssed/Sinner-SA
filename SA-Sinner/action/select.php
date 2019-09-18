d6a26341ecb0a78be7578ff09c223bc9b429ff26
Jonathan Yuri Martendal Fagundes <55164664+jonathan777x2@users.noreply.github.com>
2019-09-17 22:11:23 -0300

    Função Select *
    Select dentro da função

--- SA-Sinner/action/select.php
+++ SA-Sinner/action/select.php
@@ -1,8 +1,8 @@
 <?php
-
-$conexao = mysqli_connect('localhost','root','root','database_sinner');
-
 $pesquisa = $_POST['pesquisar']; //Pegando o POST da pag teste.php `-`
+select($pesquisa);
+function select($pesquisa){
+$conexao = mysqli_connect('localhost','root','root','database_sinner');
 $acao = mysqli_query($conexao, "SELECT * FROM usuario WHERE nome LIKE '%" . $pesquisa. "%'" ); //Pesquisando no banco `-`
 
 $array = mysqli_fetch_all($acao, MYSQLI_ASSOC);//Criando um array `-`
@@ -12,6 +12,8 @@ foreach($array as $key =>$value) { //pegando TODOS os resultados da pesquisa `-`
 }
 
 mysqli_close($conexao);
+
+}
 
 ?>
 
