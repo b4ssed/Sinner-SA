31a571750cd9d8f63b22a0701fe7ff33c3d2ef79
Matheus Sombrio <55164487+b4ssed@users.noreply.github.com>
2019-09-17 21:56:38 -0300

    Função insert
    Função insert funcionando. Realizando um "cadastro".

--- SA-Sinner/actions/insert.php
+++ SA-Sinner/actions/insert.php
@@ -1,13 +1,16 @@
 <?php 
+       
     $nome = $_POST["user"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
     insert($nome, $email, $senha);
     
-        
-    function insert($nome, $email, $password){
-        $con = mysqli_connect("localhost", "root", "root", "database_sinner");    
-        $query = mysqli_query($con,"insert into tb_usuario values(DEFAULT, '".$nome."','".$email."','".$password."','0','0','1'");
-        mysqli_close($con);   
-    }    
+
+    function insert($nome, $email, $senha){
+        $con = mysqli_connect("localhost", "root", "root", "database_sinner"); 
+        $query = mysqli_query($con,"INSERT INTO usuario VALUES(DEFAULT, '$nome', '$email', '$senha', NULL, 1)");
+        mysqli_close($con); 
+    }
+      
+       
 ?>
