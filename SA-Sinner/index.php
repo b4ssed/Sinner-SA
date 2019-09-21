77c8feea6fb664b7bbbece4b5443c67858833687
Matheus Sombrio <55164487+b4ssed@users.noreply.github.com>
2019-09-21 20:33:58 -0300

    Atualização na Estrutura de Pastas
    Adicionados os arquivos de css e js do bootstrap; adicionada pasta template; remoção de arquivos desnecessários

---
+++ SA-Sinner/index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="../actions/cadastro_usuario.php" method="post">
+            <div class="">
+                <label for="usuario">Usuário</label>
+                <input type="text" name="usuario">
+            </div>
+            <div class="">
+                <label for="email">E-mail</label>
+                <input type="email" name="email">
+            </div>
+            <div class="">
+                <label for="senha">Senha</label>
+                <input type="password" name="senha">
+            </div>
+            <button type="submit">Enviar</button>
+        </form>
+    </body>
+</html>
