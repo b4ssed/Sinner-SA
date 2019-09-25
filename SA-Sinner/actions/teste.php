faba5253df969579427b5e06eb82302181876b97
Jonathan Yuri Martendal Fagundes <55164664+jonathan777x2@users.noreply.github.com>
2019-09-24 21:44:09 -0300

    Login/validação
    Criação do login e validação do login.

--- SA-Sinner/actions/teste.php
+++
@@ -1,5 +0,0 @@
-<h2>Teste<h2>
-<form method="POST" action="select.php">
-Pesquisar <input type="text" name="pesquisar">
-<input type="submit" value="enviar">
-</form>
---
+++ SA-Sinner/includes/validar_login.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+$login = isset($_POST["login"]) ? $_POST["login"] : "";
+$senha = isset($_POST["senha"]) ? $_POST["senha"] : "";
+
+$conexao = mysqli_connect('localhost','root','root','database_sinner');
+$acao = mysqli_query($conexao, "SELECT * FROM usuario WHERE nome = '$login' AND senha = '$senha' " ); //Pesquisando no banco `-`
+
+$array = mysqli_fetch_all($acao, MYSQLI_ASSOC);//Criando um array `-`
+
+if (!empty($array)) { // VERIFICA se tem algo dentro do array (se tiver dentro do array significa que ele achou o usuario dentro do banco)
+    header("location:../index.php");
+ } else {
+    header("location:../pages/login.php?erro=100");
+ }
+
+
+mysqli_close($conexao);
+
+?>
