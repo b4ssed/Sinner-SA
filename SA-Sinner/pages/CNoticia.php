451775f28b29063a242d303702d9b91ea6637da0
Lucas-max1m14n0 <55164498+Lucas-max1m14n0@users.noreply.github.com>
2019-10-21 22:16:59 -0300

    comeco de cadastro de noticia

---
+++ SA-Sinner/pages/CNoticia.php
@@ -0,0 +1,14 @@
+
+<html>
+<form action="../includes/insert_CBanda.php" enctype="multipart/form-data"  method="post" >
+ <H1>Cadastro de Notícia</H1>
+<label >Titulo da Notícia:</label>
+<input type="text" name="TNoticia" required >
+<label > Conteúdo da Notícia</label>
+<input type="textbox" name="CNoticia">
+<label>Imagem da Noticia.</label>
+<input type="file" name="imgband" >
+<button type="submit">Enviar</button>
+</form>
+
+</html>
