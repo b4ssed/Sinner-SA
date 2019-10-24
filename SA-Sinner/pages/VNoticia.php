fabbd8c8f5411f4580a65afbf9e7cadd6cae04c6
Lucas-max1m14n0 <55164498+Lucas-max1m14n0@users.noreply.github.com>
2019-10-24 20:04:35 -0300

    0

--- SA-Sinner/pages/VNoticia.php
+++ SA-Sinner/pages/VNoticia.php
@@ -6,16 +6,13 @@
  
 $con = mysqli_connect("localhost", "root", " ", "database_sinner"); 
 
-$query = mysqli_query($con, "SELECT * FROM banda");
+$query = mysqli_query($con, "SELECT * FROM noticia");
 $dados = mysqli_fetch_all($query, MYSQLI_ASSOC);
 //var_dump($dados);
 //echo"<img src='";
 //printf ($dados[0]);
 //echo"'>"; 
-$e = count($dados);
-echo "<pre>";
-print_r($dados);
-echo "</pre>";
+
 //conectado no band
 $a=0;
 echo "<div class='card-deck'>"; 
