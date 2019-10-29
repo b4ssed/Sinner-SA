574d37195f1b32740c73d741ba7e0a0e0d4ac310
Jonathan Yuri Martendal Fagundes <55164664+jonathan777x2@users.noreply.github.com>
2019-10-28 21:43:26 -0300

    tentativa de player
    player

---
+++ SA-Sinner/pages/Player.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Player</title>
+</head>
+<body>
+
+<audio controls="controls">
+  <source src="../css/music/Kalimba.mp3" type="audio/mp3" />
+
+</audio>
+    
+</body>
+</html>
