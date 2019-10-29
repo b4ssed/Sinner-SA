34c9051327b2476a62fbcbf173157b0253eb336e
Jonathan Yuri Martendal Fagundes <55164664+jonathan777x2@users.noreply.github.com>
2019-10-28 21:45:08 -0300

    Update excluir_musica.php

--- SA-Sinner/includes/excluir_musica.php
+++ SA-Sinner/includes/excluir_musica.php
@@ -7,20 +7,8 @@
         print_r($dados);
         $ext11 = ($dados[0]['musica']);
         unlink($ext11);
-        //Cria a conexão com o banco de dados
-        
-        //Variavel que recebem o id do orçamento a ser excluído
-        
-        //Exclui o registro do banco de dados
         $query = mysqli_query($connect, "DELETE FROM musica WHERE id_musica=$idm");
-        //Fechamento da conexão com o banco de dados
         mysqli_close($connect);
-        
-
-        //exclui parquinho
-          
-
-        //Redirecionamento
        header("Location:../pages/visualizar_musica.php");
       
   ?>
