f1cc1740f32c2357c31ca3b9a9d15c7d928a2eb2
Jonathan Yuri Martendal Fagundes <55164664+jonathan777x2@users.noreply.github.com>
2019-11-04 20:45:08 -0300

    Player 10%
    123123

--- SA-Sinner/pages/Player.php
+++ SA-Sinner/pages/Player.php
@@ -2,13 +2,39 @@
 <html lang="en">
 <head>
     <title>Player</title>
+    <script>src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"</script>
 </head>
 <body>
 
-<audio controls="controls">
+<audio id="audio" preload="auto" tadindex="0" controls="">
+  <source src="../css/music/1.mp3"
+>
+</audio>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <audio controls="controls">
   <source src="../css/music/Kalimba.mp3" type="audio/mp3" />
 
-</audio>
+</audio> -->
     
 </body>
 </html>
