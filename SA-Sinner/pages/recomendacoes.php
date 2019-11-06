15a7968fee52ca83238e9ebb7ac5a4cf54201200
Matheus Sombrio <55164487+b4ssed@users.noreply.github.com>
2019-11-05 22:16:18 -0300

    Tela de recomendacoes

---
+++ SA-Sinner/pages/recomendacoes.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <?php include("../template/styles.php"); ?>
+    <title></title>
+    <script type="text/javascript">
+      document.ready()
+    </script>
+  </head>
+  <body>
+    <div class="">
+      <ul class="list-group">
+        <li class="list-group-item">
+          <input type="checkbox" value="">Dapibus ac facilisis in</label>
+        </li>
+        <li class="list-group-item">
+          <input type="checkbox" value="">Dapibus ac facilisis in</label>
+        </li>
+        <li class="list-group-item">
+          <input type="checkbox" value="">Dapibus ac facilisis in</label>
+        </li>
+        <li class="list-group-item">
+          <input type="checkbox" value="">Dapibus ac facilisis in</label>
+        </li>
+        <li class="list-group-item">
+          <input type="checkbox" value="">Dapibus ac facilisis in</label>
+        </li>
+        <li class="list-group-item">
+          <input type="checkbox" value="">Dapibus ac facilisis in</label>
+        </li>
+        <li class="list-group-item">
+          <input type="checkbox" value="">Dapibus ac facilisis in</label>
+        </li>
+      </ul>
+    </div>
+    <?php include("../template/js.php"); ?>
+  </body>
+</html>
