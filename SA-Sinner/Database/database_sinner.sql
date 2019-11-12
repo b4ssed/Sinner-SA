3a7f9e4040eb7dd776aa5af01dd3ea7e537d30fd
Matheus Sombrio <55164487+b4ssed@users.noreply.github.com>
2019-11-11 22:19:02 -0300

    Tela de acesso inicial

--- SA-Sinner/Database/database_sinner.sql
+++ SA-Sinner/Database/database_sinner.sql
@@ -51,7 +51,7 @@ ENGINE = InnoDB;
 -- Table `database_sinner`.`genero`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `database_sinner`.`genero` (
-  `id_genero` INT NOT NULL,
+  `id_genero` INT NOT NULL AUTO_INCREMENT,
   `descricao` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`id_genero`))
 ENGINE = InnoDB;
@@ -98,7 +98,7 @@ ENGINE = InnoDB;
 -- Table `database_sinner`.`musica`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `database_sinner`.`musica` (
-  `id_musica` INT NOT NULL,
+  `id_musica` INT NOT NULL AUTO_INCREMENT,
   `descricao` VARCHAR(30) NOT NULL,
   `duracao` CHAR(8) NOT NULL,
   `musica` VARCHAR(100) NOT NULL,
@@ -117,7 +117,7 @@ ENGINE = InnoDB;
 -- Table `database_sinner`.`noticia`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `database_sinner`.`noticia` (
-  `id_noticia` INT NOT NULL,
+  `id_noticia` INT NOT NULL AUTO_INCREMENT,
   `descricao` VARCHAR(50) NOT NULL,
   `conteudo` MEDIUMTEXT NOT NULL,
   `img` VARCHAR(100) NULL,
--- SA-Sinner/pages/recomendacoes.php
+++ SA-Sinner/pages/recomendacoes.php
@@ -20,17 +20,35 @@
       border-radius: 5px;
     }
     </style>
+    <!-- //https://getbootstrap.com.br/docs/4.1/components/list-group/ -->
+    <!-- <?php
+      $con = mysqli_connect("localhost", "root", "", "database_sinner");
+      $query_genero = mysqli_query($con,"SELECT COUNT(id_genero) AS result FROM genero;");
+      $array_num = mysqli_fetch_assoc($query_genero);
+      $arraynum = json_encode($array_num);
+
+      print_r($arraynum);
+      echo "<script>";
+      echo "$(document).ready(function(){";
+      echo "$('input[type='checkbox']').click(function(){";
+      echo 'if($(this).prop("checked") == true){';
+      echo '$("#1").addClass("active");';
+      echo '}';
+      echo 'else if($(this).prop("checked") == false){';
+      echo '$("#1").removeClass("active");';
+      echo '}';
+      echo '});';
+      echo '});';
+      echo '</script>';
+    ?> -->
     <script>
       $(document).ready(function(){
-        // $("#seila").click(function(){
-        //   $("#a").addClass("active");
-        // });
         $('input[type="checkbox"]').click(function(){
             if($(this).prop("checked") == true){
-                $("#a").addClass("active");
+                $("#1").addClass("active");
             }
             else if($(this).prop("checked") == false){
-                $("#a").removeClass("active");
+                $("#1").removeClass("active");
             }
         });
       });
@@ -39,29 +57,20 @@
   <body>
     <div class="container">
       <div class="mx-auto">
-        <ul class="list-group">
-          <li class="list-group-item" id="a">
-            <input type="checkbox" id="checkboxActive"  value="">Dapibus ac facilisis in</label>
-          </li>
-          <li class="list-group-item">
-            <input type="checkbox" id="checkboxActive" value="">Dapibus ac facilisis in</label>
-          </li>
-          <li class="list-group-item">
-            <input type="checkbox" id="checkboxActive" value="">Dapibus ac facilisis in</label>
-          </li>
-          <li class="list-group-item">
-            <input type="checkbox" id="checkboxActive" value="">Dapibus ac facilisis in</label>
-          </li>
-          <li class="list-group-item">
-            <input type="checkbox" id="checkboxActive" value="">Dapibus ac facilisis in</label>
-          </li>
-          <li class="list-group-item">
-            <input type="checkbox" id="checkboxActive" value="">Dapibus ac facilisis in</label>
-          </li>
-          <li class="list-group-item">
-            <input type="checkbox" id="checkboxActive" value="">Dapibus ac facilisis in</label>
-          </li>
-        </ul>
+        <div class="list-group" id="list-tab" role="tablist">1
+        <?php
+          $con = mysqli_connect("localhost", "root", "", "database_sinner");
+          $query_genero = mysqli_query($con,"SELECT * FROM genero");
+          $array_g = mysqli_fetch_all($query_genero, MYSQLI_ASSOC);
+          $arrayGenero = $array_g;
+          foreach ($arrayGenero as $key => $value) {
+            echo '<a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#lista-home" role="tab" aria-controls="home">Home</a>';
+            echo '<a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Perfil</a>';
+            echo '<a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#lista-mensagens" role="tab" aria-controls="messages">Mensagens</a>';
+            echo '<a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#lista-configuracoes" role="tab" aria-controls="settings">Configurações</a>';
+          }
+        ?>
+        </div>
       </div>
     </div>
     <?php include("../template/js.php"); ?>
