43794f90a04301c403cb7d2bd0663086effba8cd
Matheus Sombrio <55164487+b4ssed@users.noreply.github.com>
2019-11-13 22:16:56 -0300

    Tela inicial finalizada

--- SA-Sinner/includes/cadastros/cadastroRecomendacoes.php
+++ SA-Sinner/includes/cadastros/cadastroRecomendacoes.php
@@ -1,30 +1,46 @@
 <?php
-
+  session_start();
+  // echo "";
+  // // foreach ($_POST as $key => $value) {
+  // //   echo $value . ", CU: ". $key  ;
+  // //
+  // // }
+  // print_r($_SESSION["usuario"][0]["id_usuario"]);
+  // var_dump($_SESSION["usuario"]["id_usuario"]);
   $con = mysqli_connect("localhost", "root", "", "database_sinner");
-  //
+  //Conta quantos generos estão registrados no banco
   $query_genero = mysqli_query($con,"SELECT COUNT(id_genero) AS result FROM genero");
   $array_num = mysqli_fetch_assoc($query_genero);
-  //
+  //Array para armazenar registros enviados pelo form
   $arrayDados = array();
   $a = 0;
+  //While para armazenar os registros do form
   while ($a <= $array_num['result']) {
-    switch ($a) {
-      case $a:
-        if (isset($_POST[$a])){
-            $arrayDados[] = $_POST[$a];
-        }
-        break;
-
-      default:
-        // code...
-        break;
+    if (isset($_POST[$a])){
+        $arrayDados[] = $_POST[$a];
     }
     $a++;
   }
+  //Armazenar no banco de dados
   if (isset($arrayDados)) {
-    $query_usuario_genero = mysqli_query($con,"INSERT INTO usuario_genero VALUES(DEFAULT, )");
+    //Quantidade de generos selecionados pelo usuário
+    $qtdDados = count($arrayDados);
+    $b = 0;
+    while ($b < $qtdDados) {
+      //Pegar o id do usuário logado atualmente no site
+      //E cadastrar cada genero selecionado pelo usuário
+      foreach ($_SESSION['usuario'] as $key => $value) {
+        //Selecionar id dos generos
+        $query_genero1 = mysqli_query($con,"SELECT id_genero FROM genero WHERE descricao= '$arrayDados[$b]'");
+        $nomeGenero = mysqli_fetch_array($query_genero1);
+        //Inserir os dados generos no banco
+        $query_usuario_genero = mysqli_query($con, "INSERT INTO usuario_genero VALUES(DEFAULT, '{$value['id_usuario']}', '{$nomeGenero['id_genero']}')");
+      }
+      $b++;
+    }
+    mysqli_close($con);
+    header("Location:../../index.php");
   }else{
 
   }
-  print_r($arrayDados);
 ?>
--- SA-Sinner/pages/recomendacoes.php
+++ SA-Sinner/pages/recomendacoes.php
@@ -29,27 +29,49 @@
       $query_genero = mysqli_query($con,"SELECT COUNT(id_genero) AS result FROM genero;");
       $array_num = mysqli_fetch_assoc($query_genero);
       $arraynum = json_encode($array_num);
-
     ?>
     <script>
-      $(document).ready(function(){
-        $('input[type="checkbox"]').click(function(){
-            if($(this).prop("checked") == true){
-              $("#a").addClass("active");
-              $("#btnSubmit").removeClass("disabled");
-            }
-            else if($(this).prop("checked") == false){
-                $("#a").removeClass("active");
-                $("#btnSubmit").addClass("disabled");
-            }
+      $(document).ready(function () {
+        $('#myCheckbox').click(function () {
+          $('#myButton').prop("disabled", !$("#myCheckbox").prop("checked"));
         });
-        // $("#checkboxActive").checked(function(){
-        //
-        // });
       });
+      //   $('#myCheckbox').click(function (){
+      //       let rabo = $('#myCheckbox').val();
+      //       console.log(rabo)
+      //     $(`#${rabo}`).addClass("active");
+      //   });
+      // });
+      //
+      //
+      //
+      //
+      //
+      //
+      //   $('input[type="checkbox"]').click(function(){
+      //       if($(this).prop("checked") == true){
+      //         $("#1").addClass("active");
+      //         $("#2").addClass("active");
+      //         $("#3").addClass("active");
+      //         $("#4").addClass("active");
+      //
+      //         // $("#btnSubmit").removeClass("disabled");
+      //         $('#cu').prop('disabled', false);
+      //        }
+      //       else if($(this).prop("checked") == false){
+      //           $("#1").removeClass("active");
+      //           $("#2").removeClass("active");
+      //           $("#3").removeClass("active");
+      //           $("#4").removeClass("active");
+      //
+      //           // $("#btnSubmit").addClass("disabled");
+      //           $('#cu').prop('disabled', true);
+      //       }
+      //   });
+      //     });
 
-
-
+      let gya = "shausha"
+      console.log(`seu cu, ${gya} asjgdsjhsvgdfsjdfhksahfgksajhdgkjhsadjhg`);
     </script>
   </head>
   <body>
@@ -65,15 +87,14 @@
               $array_g = mysqli_fetch_all($query_genero, MYSQLI_ASSOC);
               $arrayGenero = $array_g;
               foreach ($arrayGenero as $key => $value) {
-
-                echo '<li class="list-group-item" id="">';
-                echo '<input type="checkbox" name="'.$value['id_genero'].'" value="'.$value["descricao"].'">'.$value["descricao"].'</label>';
+                echo '<li class="campinho list-group-item" id="'.$value['id_genero'].'">';
+                echo '<input type="checkbox" id="myCheckbox" name="'.$value['id_genero'].'" value="'.$value['descricao'].'">'.$value["descricao"].'</label>';
                 echo '</li>';
               }
             ?>
             </ul>
           </div>
-          <li class="list-group-item"><center><button type="submit" class="btn btn-dark disabled" id="btnSubmit">Enviar</button></center></li>
+          <li class="list-group-item"><center><input disabled="disabled" type="submit" id="myButton" name="" value="Enviar"></center></li>
         </form>
       </div>
     </div>
