78efe64abdd9a4fa7922a79906fc3e0e08ea20c4
Lucas-max1m14n0 <55164498+Lucas-max1m14n0@users.noreply.github.com>
2019-11-19 20:03:19 -0300

    .

--- SA-Sinner/pages/cadastros/cadastroNoticia.php
+++ SA-Sinner/pages/cadastros/cadastroNoticia.php
@@ -1,18 +1,24 @@
 <html>
-<form action="../../includes/cadastros/cadastroNoticia.php?aa='1'" enctype="multipart/form-data"  method="post" >
- <H1>Cadastro de Notícia</H1>
-<label >Titulo da Notícia:</label>
-<input type="text" name="TNoticia" required >
-    <?php include("../../template/styles.php"); ?>
-    <link rel="stylesheet" href="../../css/styleCadastro.css">
+
+    <head>
+        <?php include("../../template/styles.php"); ?>
+        <link rel="stylesheet" href="../../css/styleCadastro.css">
+    </head>
+   
     <div class="wrapper">
-    <?php include("../../template/navbar.php"); ?>
-    <div id="content" class="container">
-    <div class="containerCadastro">
-    <hr />
+        <?php include("../../template/navbar.php"); ?>
+            <div id="content" class="container">
+                <div class="containerCadastro">
+                    <hr/>
+    <form action="../../includes/cadastros/cadastroNoticia.php" enctype="multipart/form-data"  method="post" >
                   <center>
                   <H2>Cadastro de Noticia</H2>
                   </center>
+                  <div>
+                  <label>Titulo da notícia:</label>
+                  <input type="text" id="TNoticia" required pattern={*,3} class="form-control" name="TNoticia" placeholde="Lançamento de novo album" >
+                  </div>
+                  <br>
                   <div class="form-group">
                     <label>Genero:</label>
                 <select name="genero" id='genero'>
@@ -35,7 +41,7 @@
     <label>Imagem da Noticia.</label>
     <input type="file" name="imgband" >
     </div>
-    <button  id='submitt' type="submit" class="form-control btn btn-dark"> noticia</button>
+    <button  id='submitt' type="submit" class="form-control btn btn-dark">enviar</button>
 </form>
 </div>
 </div>
--- SA-Sinner/pages/visualizar/visualizarNoticia.php
+++ SA-Sinner/pages/visualizar/visualizarNoticia.php
@@ -1,40 +1,62 @@
 <html>
-<head>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Visualizar Noticia</title>
+        <?php include("../../template/styles.php"); ?>
+        <style>
+            .containerCadastro{
+            width:800px;
+            padding: 15px;
+            border-radius: 10px;
+            background: #fff;
+            }
+            .containerPrincipal{
+            background: #b2bec3;
+            }
 
-
-
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
+        </style>
+    </head>
+    <thead>
+             <tr>
+               <th scope="col">Notícia</th>
+             </tr>
+           </thead>
+            <tr class="">
+                    <th>Imagem</th>
+                    <th>Titulo</th>
+                    <th>Genero</th>
+                    <th></th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
 <?php
  
 $con = mysqli_connect("localhost", "root", "", "database_sinner"); 
-
 $query = mysqli_query($con,"SELECT * FROM noticia");
 $dados = mysqli_fetch_all($query, MYSQLI_ASSOC);
 $query2 = mysqli_query($con,"SELECT * FROM genero");
 $dados2 = mysqli_fetch_all($query2, MYSQLI_ASSOC);
-
-//echo"<img src='";
-
-//echo"'>"; 
-
-//conectado no band
 $a=0;
 echo "<div class='card-deck'>"; 
-foreach ($dados as $key => $value) {
-    
-    echo "<a href='jornal.php?id=".($value['id_noticia'])."'> <div class='card' style='width: 18rem;'>
-    
-    <div class='card-body' >
-    
-    <h3>".$value['descricao']."</h3>";
-    echo '<a href="../includes/ExcluirBanda.php?idb='.($value['id_noticia']).'"><button>Excluir</button></a>';
-    echo '<a href="../editar/noticias/editarnoticia.php?idb='.($value['id_noticia']).'"><button>adicionar noticia</button></a>';  
-   echo" </div>
-    </div>
-    </a>";
-}
+
+
+        foreach ($dados as $key => $value) {
+            echo"<tr>";
+            echo "<td><img src=".$value['img']."></td>";
+            echo "<td>".$value['descricao']."</td>";
+            echo "<a href='jornal.php?id=".($value['id_noticia'])."'> <div class='card' style='width: 18rem;'>
+            
+            <div class='card-body' >
+            
+            <h3>".$value['descricao']."</h3>";
+            echo '<a href="../includes/ExcluirBanda.php?idb='.($value['id_noticia']).'"><button>Excluir</button></a>';
+            echo '<a href="../editar/noticias/editarnoticia.php?idb='.($value['id_noticia']).'"><button>adicionar noticia</button></a>';  
+        echo" </div>
+            </div>
+            </a>";
+        }
 ?>
 </div>
 
