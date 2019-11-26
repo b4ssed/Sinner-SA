955da998db1fe08b128d8ab270f472b449a5023d
Matheus Sombrio <55164487+b4ssed@users.noreply.github.com>
2019-11-25 22:19:16 -0300

    Tela Inicial do Usuário
    Inicio dos processos do banco de dados e estilização dos cards.

--- SA-Sinner/pages/home.php
+++ SA-Sinner/pages/home.php
@@ -1,3 +1,9 @@
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<?php
+  //Select
+  $con = mysqli_connect("localhost", "root", "", "database_sinner");
+  
+?>
 <div class="conteudo">
   <div class="card">
     <div class="card-header">
@@ -12,12 +18,21 @@
   <br>
   <div class="row">
     <div class="col-4">
-      <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="css/images/image01pegboardnerds.jpg" alt="Imagem de capa do card">
-        <div class="card-body">
-          <h5 class="card-title">Notícia 1</h5>
-          <p class="card-text">Um exemplo de texto rápido para construir o Notícia e fazer preencher o conteúdo do card.</p>
-          <a href="#" class="btn btn-primary">Visitar</a>
+      <div class="card card-cascade wider">
+        <!-- Card image -->
+        <div class="view view-cascade overlay">
+          <img  class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" alt="Card image cap">
+          <a href="#!">
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+        <!-- Card content -->
+        <div class="card-body card-body-cascade text-center pb-0">
+          <!-- Title -->
+          <h4 class="card-title"><strong>Alison Belmont</strong></h4>
+          <!-- Subtitle -->
+          <h5 class="blue-text pb-2"><strong>Graffiti Artist</strong></h5>
+          <p class="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam. </p>
         </div>
       </div>
     </div>
