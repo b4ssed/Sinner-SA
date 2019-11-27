569ec26aa09f13656f1d5d5eb33da70215e18e48
Jonathan Yuri Martendal Fagundes <55164664+jonathan777x2@users.noreply.github.com>
2019-11-26 22:12:27 -0300

    Next e Prev
    tentando fazer o Next e Prev do player

--- SA-Sinner/includes/cadastros/cadastrarMusica.php
+++ SA-Sinner/includes/cadastros/cadastrarMusica.php
@@ -4,16 +4,17 @@
  $duracao = $_POST['duracao'];
  $album = $_POST['album'];
 
-    $destino= '../../css/music/';
+    
     $ext = strtolower(substr($_FILES['music']['name'],-4));
     if($ext==".mp3"||$ext==".wma"||$ext==".aac"||$ext==".ogg"){
         $new_name = $_FILES['music']['name'];
+        $destino= '../css/music/'.$new_name;
         $caminho="../../css/music/".$new_name;
     // echo $caminho;
     // print_r($_FILES['music']);
         move_uploaded_file($_FILES['music']['tmp_name'], $caminho);
         $con = mysqli_connect("localhost", "root", "", "database_sinner");
-        $query_insert = mysqli_query($con,"INSERT INTO musica VALUES(DEFAULT, '$nmusica', '$duracao', '$caminho', $album)");
+        $query_insert = mysqli_query($con,"INSERT INTO musica VALUES(DEFAULT, '$nmusica', '$duracao', '$destino', $album)");
         mysqli_close($con);
         echo "<script> alert('Musica cadastrada com sucesso !'); window.location.href ='../../pages/cadastros/cadastroMusica.php'; </script>" ;
     }else{
--- SA-Sinner/pages/Player.php
+++ SA-Sinner/pages/Player.php
@@ -1,51 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+<!--//https://codepen.io/markhillard/pen/Hjcwu -->
   <head>
       <title>Player</title>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
       <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=es6,Array.prototype.includes,CustomEvent,Object.entries,Object.values,URL"></script>
       <script src="https://unpkg.com/plyr@3"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src=>"https://cdnjs.cloudflare.com/ajax/libs/html5media/1.1.8/html5media.min.js"</script>
+      <script src=>"https://cdnjs.cloudflare.com/ajax/libs/plyr/3.5.6/plyr.min.js"</script>
+      <script src=>""</script>
       <link rel="stylesheet" href="https://unpkg.com/plyr@3/dist/plyr.css"/>
+      <!-- Nav Bar -->
       <?php include("../template/styles.php"); ?>
-      
+      <!-- Fim Bar -->
+
+      <!-- Começo CSS -->
       <style>
 
         ul{list-style: none;padding: 0px}
+       
         a{text-decoration: none;color: #444;font-family: arial}
+       
         li:hover{background: #eee;border-bottom: solid 1px #692273;}
+       
         li{width: 20%; padding: 5px; border-bottom: solid 1px #ccc;}
+        
         .active a{color:#692273; padding-left: 1px; font-style: italic;}
-
 
         .plyr--audio .plyr__controls{
           background:black;
-          
-
-
         }
-      </style>
-      <style>
-          #rodape {
+        
+        #rodape {
             position: absolute;
             bottom: 0;
             width: 100%;
           }
 
-
-
+         #tracks {
+            font-size:60px;
+            position:relative;
+            text-align:center;
+            }
       </style>
-      <script>
+      <!-- Fim CSS -->
 
+      <!-- Começo JavaScript -->
+      <script>
       $(document).ready(function(){
         inicio();
-
         const player = new Plyr('audio', {});
-
-        // Expose player so it can be used from the console
         window.player = player;
-
       });
-
 
       function inicio(){
         var corrente = 0;
@@ -55,22 +63,23 @@
         var len = tracks.length -1;
         audio[0].play();
         playlist.find("a").click(function(e){
-          e.preventDefault();
-          link = $(this);
-          corrente = link.parent().index();
-          run(link, audio[0]);
+            e.preventDefault();
+            link = $(this);
+            corrente = link.parent().index();
+            run(link, audio[0]);
         });
           audio[0].addEventListener("ended", function(e){
             corrente++;
             if(corrente == len){
-              corrente = 0;
-              link = playlist.find("a")[0];
+                corrente = 0;
+                link = playlist.find("a")[0];
             }else{
-              link = playlist.find("a")[corrente];
-            }
-            run($(link), audio[0]);
+                link = playlist.find("a")[corrente];
+              }
+              run($(link), audio[0]);
           })
       }
+
         function run(link, player){
           player.src = link.attr("href");
           par = link.parent();
@@ -79,42 +88,64 @@
           player.play();
         }
 
+        btnNext = $('#btnNext').on('click', function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+              })
 
 
       </script> 
+      <!-- FIM JavaScript -->
   </head>
+
   <body>
-    
+  <!-- Começo interação no BD -->
     <?php
       $con = mysqli_connect("localhost","root","","database_sinner");
       $query = mysqli_query($con, "SELECT * FROM musica");
       $arr = mysqli_fetch_all($query, MYSQLI_ASSOC);
-
-      
     ?>
-    <!-- Lista de musica -->
-    
-    <!-- Player-->
+    <!-- FIM interação no BD -->
+
     <div class="wrapper">
       <?php include("../template/navbar.php"); ?> 
-      <div id="content">
-      <?php include("../includes/btnNavbar.php"); ?> 
-        <ul id="playlist"> 
-          <?php
-              foreach ($arr as $key => $value) {
-                echo "<li><a href='".$value["musica"]."'>".$value["descricao"]."</a></li>";
-              }
-          ?>
-          </ul>
-          
-      </div>
-      <div id='content2'>
-          <div id=rodape> 
-            <audio class="tu" id="audio" crossorigin playsinline>
-              <source src="">
-            </audio>
-          </div>
+      <div id="content" style="padding: 0px">
+        <div style="padding: 40px">
+          <?php include("../includes/btnNavbar.php"); ?> 
+            <ul id="playlist"> 
+              <?php
+                  $idmusicaatual;
+                  $idmusicaatual+=1;
+                  $idmusicaatual-=1;
+                  foreach ($arr as $key => $value) {
+                    
+                    echo "<a href='".$value["musica"]."'><li>".$value["descricao"]."</li></a>";
+                  }
+              ?>
+              </ul>
         </div>
+    <!-- Começo Player-->
+        <div id="tracks">
+           <button id="btnPrev">&vltri;</button><button id="btnNext">&vrtri;</button>
+        </div> 
+
+        <div id="rodape" >   
+          <audio class="tu" id="audio" crossorigin playsinline >
+            <source src="">
+          </audio>
+        </div>
+    <!-- FIM Player-->
+      </div>
+
     </div>
     <?php include("../template/js.php"); ?>
   </body>
