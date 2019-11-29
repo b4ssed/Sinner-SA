c84defcdc9e683086d34ee85ea7108147c3ca75e
Matheus Sombrio <55164487+b4ssed@users.noreply.github.com>
2019-11-28 21:16:47 -0300

    Conflitos com a Nav e Sidebar
    Problemas com sessão resolvidos

--- SA-Sinner/index.php
+++ SA-Sinner/index.php
@@ -1,14 +1,12 @@
-
 <html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <?php session_start(); ?>
         <title></title>
         <?php
-          session_start(); 
-          
           if (isset($_SESSION['usuario'])) {
             include("template/styles.php");
           }
